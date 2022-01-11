c7d56297ea53041fb4d1235e352bcc7372bc30e9
rserafin <rserafin.air@gmail.com>
2022-01-11 15:47:59 +0100

    Add new class

---
+++ Data/Model.py
@@ -0,0 +1,10 @@
+import requests
+from xml.etree import ElementTree
+from bs4 import BeautifulSoup
+
+
+class Model:
+    def __init__(self, model_link):
+        self.__brand = brand
+        self.__model = model
+        self.names = data_model['names']
