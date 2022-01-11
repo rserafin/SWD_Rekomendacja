3efb43b9bd731a77f070b336a8b57d39a98d2f75
rserafin <rserafin.air@gmail.com>
2022-01-11 14:54:05 +0100

    data_test

---
+++ data_test.py
@@ -0,0 +1,8 @@
+data_test = {
+    'brand': '', 'model': '', 'notes': '', 'names': '', 'standards': '',
+    'water': '', 'dimensions': '', 'weight': '', 'display': '', 'display_protection': '',
+    'battery': '', 'charging': '', 'memory': '', 'memory_useres': '',
+    'RAM': '', 'memory_cards': '', 'system': '', 'procesor': '', 'first_camera': '',
+    'second_camera': ''}
+
+data_base = [data_test]
